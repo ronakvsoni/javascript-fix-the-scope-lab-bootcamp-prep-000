c5c7cce137cef95c0c32e63d912ef0e0f3d17b76
Ronak V Soni <ronakvsoni@gmail.com>
2020-05-07 21:46:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -42,17 +42,3 @@ console.log(theFunk);
 
 
 
-
-
-function outerFunction() {
-  var innerVariable = "I'm sort of a secret.";
- 
-  return function innerScope() {
-    var inaccessible = "Nothing can touch me.";
- 
-    return innerVariable;
-  }
-}
-
-var myScope = outerFunction()
-console.log (myScope());
